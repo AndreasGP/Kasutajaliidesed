a753e9fb22dd76d8d347a15839e66eaa8042e6b9
KristjanP94 <kristjan.perli@gmail.com>
2015-04-22 12:11:45 +0300

    Test commit.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
     <?php include 'navbar.php' ?>
     <div class="container">   
-        <div class="jumbotron"><h1>WELCUM 2 valimised</h1></div>
+        <div class="jumbotron"><h1>WELCUM 3 valimised</h1></div>
     </div>
 </body>
 <?php include 'footer.php' ?>
