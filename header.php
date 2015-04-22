67aa62ea89f63a4c59b12f46af05398dd56ca79c
AndreasGP <aaatttsss@hotmail.com>
2015-04-22 13:14:14 +0300

    Added flags

--- header.php
+++ header.php
@@ -3,5 +3,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <!-- <meta http-equiv="Cache-control" content="public"></meta> -->
         <link rel="stylesheet" href="/e-valimised/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/e-valimised/css/styles.css" />
         <script type="text/javascript" src="/e-valimised/js/jquery-1.11.2.min.js"></script>
         <script type="text/javascript" src="/e-valimised/js/bootstrap.min.js"></script>
--- navbar_sisselogimata.php
+++ navbar_sisselogimata.php
@@ -3,13 +3,20 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="/e-valimised">E-valimised 2015</a>
         </div>
-        <div class="navbar-collapse collapse navbar-right">
+        <div class="navbar-collapse collapse pull-right">
             <ul class="nav navbar-nav">
-                <li><a href="/e-valimised/abi">Abi</a></li>
-                <li><a href="/e-valimised/kandidaadid">Kandidaadid</a></li>
-                <li><a href="/e-valimised/haaletamine">Hääletamine</a></li>
-                <li><a href="/e-valimised/sisene">Sisene ID-kaardiga</a></li>
+                <li><a href="/e-valimised/abi.php">Abi</a></li>
+                <li><a href="/e-valimised/kandidaadid.php">Kandidaadid</a></li>
+                <li><a href="/e-valimised/haaletamine.php">Hääletamine</a></li>
+                <li><a href="/e-valimised/sisene.php">Sisene ID-kaardiga</a></li>
+            </ul>
+            <br>
+            <ul class="nav navbar-nav pull-right">
+                <li><img src="/e-valimised/assets/ee.svg" class="lipp" width="40px" alt="Eesti keel" ></img>
+                <li><img src="/e-valimised/assets/ru.svg" class="lipp" width="40px" alt="русский"><div class="mitteaktiivne"></div></img>
+                <li><img src="/e-valimised/assets/gb.svg" class="lipp" width="40px" alt="English"><div class="mitteaktiivne"></div></img>
             </ul>
         </div>
+        <br>
     </div>
 </nav>
