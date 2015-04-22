56fcdf7a4eda6953a7f8b2bbdfedb35137b4e422
KristjanP94 <kristjan.perli@gmail.com>
2015-04-22 12:35:58 +0300

    Footer

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
-<footer>
-Footer
+<footer class="footer">
+    <nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        <div class="navbar-header">
+            <p class="navbar-brand">Eesti E-Valimised 2016</p>
+            <p class="navbar-brand"></p>
+            <p class="navbar-brand">Abitelefon: 112</p>
+            <p class="navbar-brand"></p>
+            <p class="navbar-brand">E-mail: hüdse@kek.ee</p>
+        </div>
+    </div>
+    </nav>
+    
 </footer>
